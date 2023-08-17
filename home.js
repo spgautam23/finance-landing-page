009b61e0563c27ce53f328a4872c69408b113451
spgautam23 <spgautam6387@gmail.com>
2023-08-17 23:21:12 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,90 @@
+var button1= document.getElementById("myButton1");
+
+function openLink1(){
+    var link="https://volta.finance/wrapper";
+
+    window.open(link, '_blank');
+}
+
+button1.onclick= openLink1;
+
+var button2= document.getElementById("myButton2");
+
+function openLink2(){
+    var link="https://volta.finance/vaults";
+
+    window.open(link, '_blank');
+}
+
+button2.onclick= openLink2;
+
+var icon1= document.getElementById("icon1");
+
+function openLink3(){
+    var link="https://twitter.com/volta_protocol";
+
+    window.open(link, '_blank');
+}
+
+icon1.onclick= openLink3;
+
+var icon2= document.getElementById("icon2");
+
+function openLink4(){
+    var link="https://discord.com/invite/qYEWSVahFh";
+
+    window.open(link, '_blank');
+}
+
+icon2.onclick= openLink4;
+
+var icon3= document.getElementById("icon3");
+
+function openLink5(){
+    var link="https://t.me/VoltaFinance";
+
+    window.open(link, '_blank');
+}
+
+icon3.onclick= openLink5;
+
+var link1= document.getElementById("link1");
+
+function openLink6(){
+    var link="https://volta.finance/wrapper";
+
+    window.open(link, '_blank');
+}
+
+link1.onclick= openLink6;
+
+var link2= document.getElementById("link2");
+
+function openLink7(){
+    var link="https://volta.finance/vaults";
+
+    window.open(link, '_blank');
+}
+
+link2.onclick= openLink7;
+
+
+var link3= document.getElementById("link3");
+
+function openLink8(){
+    var link="https://volta.finance/swap";
+
+    window.open(link, '_blank');
+}
+
+link3.onclick= openLink8;
+
+var link4= document.getElementById("link4");
+
+function openLink9(){
+    var link="https://volta.finance/stake";
+
+    window.open(link, '_blank');
+}
+
+link4.onclick= openLink9;
